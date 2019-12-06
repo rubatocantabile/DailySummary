15e91217e158f8739a36e9240198e853bbeeb50f
Chorong Han <rubatocantabile@gmail.com>
2019-12-06 14:02:36 +0900

    플라스크와 연결 전 상태

--- client/src/App.js
+++ client/src/App.js
@@ -92,6 +92,17 @@ class App extends Component {
     });
   };
 
+  writeBtnClicked = e => {
+    console.log(e)
+    e.preventDefault();
+    e.stopPropagation();
+    if( {isLogined : this.state.isLogined } === false) {
+      this.props.history.push('/login')
+    } else {
+      this.props.history.push('post/write')
+    }
+  }
+
   render () {
     return (
       <LoginContext.Provider value={{
@@ -117,11 +128,9 @@ class App extends Component {
               </Link>
             </div>
               <Menu theme="light" mode="inline" className="one-nav">
-                <Menu.Item key="1">
-                  <Link to="/post/write">
+                <Menu.Item key="1" className="menuWrite"  onClick={this.writeBtnClicked}>
                     <Icon type="form" />
                     <span className="nav-text">글쓰기</span>
-                  </Link>
                 </Menu.Item>
                 <SubMenu
                   key="Sub1"
@@ -215,3 +224,223 @@ class App extends Component {
   };
 };
 export default withRouter(App);
+
+
+//로그인, 로그아웃 등 기능하는 버젼/ 사이드바 메뉴 수정 전 임시 주석처리
+// import React, { Component, Fragment } from 'react'
+// import './App.scss'
+// import Home from './components/Home/Home'
+// import HeaderLayout from './components/HeaderLayout'
+// import Write from './components/Post/Write/Write'
+// import { Route, Switch, Link, withRouter, Redirect } from 'react-router-dom'
+// import NormalLoginForm from './components/Login/Login'
+// import { Modal, Layout, Menu, Icon, DatePicker } from 'antd'
+// import PostList from './components/Post/PostList/PostList'
+// import NotFound from './components/NotFound'
+// import Settings from './components/Settings'
+// import SignUp from './components/Login/SignUp'
+// import Unsubscribe from './components/Login/Unsubscribe'
+// import Review from './components/Post/Write/Review'
+// import Summary from './components/Post/Summary/Summary'
+// import axios from 'axios'
+// import { LoginContext } from './contexts/login'
+
+// const { Sider, Content, Footer } = Layout
+// const { SubMenu } = Menu
+// const { MonthPicker } = DatePicker;
+
+// const config = require('./config');
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       collapsed: false,
+//       visible: false,
+//       isLogined: localStorage.getItem('token') ? true : false,
+//     }
+//     // this.onChange = this.onChange.bind(this)
+//     //this.showModal = this.showModal.bind(this)
+//     //this.handleOk = this.handleOk.bind(this)
+//     //this.handleCancel = this.handleCancel.bind(this)
+//   }
+
+//   setIsLogined = (isLogined) => {
+//     this.setState({ isLogined })
+//   }
+
+//   onCollapse = (collapsed) => {
+//     console.log(collapsed)
+//     this.setState({ collapsed })
+//   }
+
+//   pickedMonth = (date, dateString) => {
+//     const dateStr = dateString.replace(/-/g, '');
+//     const url = '/posts/' + dateStr.substring( 0, 4 ) + 
+//                 '/' + dateStr.substring( 4, 6 );
+//     console.log(url);
+//     this.props.history.push(url);
+//   }
+
+//   pickedDate = (date, dateString) => {
+//     const dateStr = dateString.replace(/-/g, '');
+//     const url = '/summary/' + dateStr.substring( 0, 4 ) + 
+//                 '/' + dateStr.substring( 4, 6 ) + 
+//                 '/' + dateStr.substring( 6, 8 );
+//     console.log(url);
+//     this.props.history.push(url);
+//   }
+
+//   showModal = () => {
+//     console.log("showModal");
+//     this.setState({
+//       visible: true,
+//     });     
+//   };
+
+//   handleOk = e => {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     console.log(e);
+//     localStorage.removeItem("token")
+//     this.setState({
+//       visible: false,
+//       isLogined: false,
+//     });
+//     this.props.history.push('/')
+//   };
+  
+//   handleCancel = e => {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     console.log('cancel clicked');
+//     console.log(this.state);
+//     this.setState({
+//       visible: false,
+//     });
+//   };
+
+//   render () {
+//     return (
+//       <LoginContext.Provider value={{
+//         setIsLogined: this.setIsLogined,
+//       }}>
+//         <Fragment>
+//         {/* Sider, Header, Footer는 모든 화면에 보여진다.  */}
+//           <Layout>
+//             <Sider
+//               breakpoint="lg"
+//               collapsedWidth="0"
+//               onBreakpoint={broken => {
+//                 console.log(broken)
+//               }}
+//               onCollapse={(collapsed, type) => {
+//                 console.log(collapsed, type)
+//               }}
+//               className="one-sidebar"
+//             >
+//             <div className="one-menu-logo flex flex-center"  >
+//               <Link to="/">
+//                 <span>몽 글</span>
+//               </Link>
+//             </div>
+//               <Menu theme="light" mode="inline" className="one-nav">
+//                 <Menu.Item key="1">
+//                   <Link to="/post/write">
+//                     <Icon type="form" />
+//                     <span className="nav-text">글쓰기</span>
+//                   </Link>
+//                 </Menu.Item>
+//                 <SubMenu
+//                   key="Sub1"
+//                   title={
+//                     <span>
+//                       <Icon type="read" />
+//                       <span>본문</span>
+//                     </span>
+//                   }
+//                 >
+//                   <Menu.Item key="2">
+//                       <MonthPicker
+//                         onChange={this.pickedMonth}
+//                         placeholder="Select month" />
+//                   <span className="nav-text"></span>
+//                   </Menu.Item>
+//                 </SubMenu>
+//                 <SubMenu
+//                   key="Sub2"
+//                   title={
+//                     <span>
+//                       <Icon type="edit" />
+//                       <span>요약</span>
+//                     </span>
+//                   }
+//                 >
+//                   <Menu.Item key="3">
+//                       <DatePicker
+//                         onChange={this.pickedDate}
+//                       />
+//                   <span className="nav-text"></span>
+//                   </Menu.Item>
+//                 </SubMenu>
+//                 <Menu.Item key="4">
+//                   <Link to="/setting">
+//                     <Icon type="setting" />
+//                     <span className="nav-text">설정</span>
+//                   </Link>
+//                 </Menu.Item>
+//                 <Menu.Item key="5"
+//                   className="one-logout"
+//                   onClick={this.showModal}
+//                   >
+//                     <Icon type="logout" />
+//                     <span className="nav-text">
+//                       로그아웃
+//                     </span>
+//                     <Modal
+//                       title="Basic Modal"
+//                       visible={this.state.visible}
+//                       onOk={this.handleOk}
+//                       onCancel={this.handleCancel}
+//                     >
+//                       로그아웃 하시겠습니까?
+//                     </Modal>   {/* 로그아웃 클릭시 로그인 버튼으로 전환 및 연동 부분 추가 */}
+//                 </Menu.Item>
+//               </Menu>
+//             </Sider>
+//             <Layout className="one-main">
+//               <HeaderLayout />
+//               <Content className="Content-section-layout one-content">
+//                 {/* {
+//                   this.state.isLogined ? (  */}
+//                     <Switch>
+//                       <Route exact path="/" component={Home} />
+//                       <Route path="/posts/:year/:month/:day" component={PostList} />
+//                       <Route path="/posts/:year/:month" component={PostList} />
+//                       <Route path="/posts/:year" component={PostList} />
+//                       <Route path="/posts" component={PostList} />
+//                       <Route path="/post/write" component={Write} />
+//                       <Route path="/post/:view" component={Review} />
+//                       <Route path="/summary/:year/:month/:day" component={Summary} />
+//                       <Route path="/summary" component={Summary} />
+//                       <Route path="/setting" component={Settings} />
+//                       <Route path="/signup" component={SignUp} />
+//                       <Route path="/login" component={NormalLoginForm} />
+//                       <Route path="/unsubscribe" component={Unsubscribe} />
+//                       <Route component={NotFound} />
+//                     </Switch>
+//                   {/* // ) : (
+//                   //   <NormalLoginForm />
+//                   // )
+//                 } */}
+//               </Content>
+//               <Footer style={{ textAlign: 'center' }}>ㅁㅗㅇㄱㅡㄹ ©2019 Created by 한:글</Footer>
+//             </Layout>
+//           </Layout>
+//         </Fragment>
+//       </LoginContext.Provider>
+//     )          
+//   };
+// };
+// export default withRouter(App);
--- client/src/components/Post/Write/Write.js
+++ client/src/components/Post/Write/Write.js
@@ -1,106 +1,5 @@
-//AI서버와 테스트용
+// //AI서버와 테스트용
 
-import React, { Component } from 'react';
-import { Input, Button, Modal } from 'antd';
-import LiveClock from './LiveClock';
-import Emotion from './Emotion';
-import { withRouter } from 'react-router';
-import axios from 'axios';
-
-const { TextArea } = Input;
-const config = require('../../../config');
-
-class Write extends Component {
-  constructor(props) {
-    super(props) 
-
-    this.state = {
-      visible: false,
-      paragraph: "",
-      affectivity: "",
-      keyword: "",
-      onesentence: "",
-    }
-    this.paragraphChanged = this.paragraphChanged.bind(this)
-    this.selectedEmotion = this.selectedEmotion.bind(this)
-  }  
-  
-  showModal = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  handleOk = e => { 
-    axios.post(config.aiServer + "/summary", {
-      // paragraph: this.state.paragraph,
-      // affectivity: this.state.affectivity,
-      text: [this.state.paragraph],
-      emotion: Number(this.state.affectivity),
-    })
-    .then((response) => {       
-      console.log(response) 
-      this.setState({
-        visible: true,
-        // paragraph: "",
-        // affectivity: "",
-        keyword: response.data.keyword,
-        onesentence: response.data.onesentence,       
-      })
-      // this.props.history.push('/posts')       
-    })
-    .catch((error) => {
-      alert("에러 발생: " + error.message)
-      console.error(error)
-    })
-  }
-
-  handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
-
-  paragraphChanged(event) {
-    this.setState({
-      paragraph: event.target.value,
-    })
-  }
-
-  selectedEmotion(e) {
-    this.setState({
-      affectivity: e.target.value
-    })
-  }
-
-  render() {
-
-    return (
-      <div className="one-post-write">
-        <div className="one-liveClock-container">
-          <LiveClock />
-        </div> 
-        <TextArea className="one-textarea" 
-          placeholder="...그래서 오늘은 어땠어?"
-          value={this.state.paragraph}
-          onChange={this.paragraphChanged}  />
-        <div className="one-post-btn-container flex">
-          <Emotion clickHandler={this.selectedEmotion}/>
-          <Button type="primary" onClick={this.handleOk} className="btn btn-submit">저장</Button>
-          <Modal title="글이 완성되었습니다." visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel} >
-{/*  리뷰 페이지로 이동 */}
-            {"키워드: " + this.state.keyword + "    \n" +
-             "한줄: " + this.state.onesentence}
-          </Modal>       
-        </div>
-      </div>
-    )
-  }
-}
-export default withRouter(Write) 
-
-//server랑 연결 원본
 // import React, { Component } from 'react';
 // import { Input, Button, Modal } from 'antd';
 // import LiveClock from './LiveClock';
@@ -119,6 +18,8 @@ export default withRouter(Write)
 //       visible: false,
 //       paragraph: "",
 //       affectivity: "",
+//       keyword: "",
+//       onesentence: "",
 //     }
 //     this.paragraphChanged = this.paragraphChanged.bind(this)
 //     this.selectedEmotion = this.selectedEmotion.bind(this)
@@ -131,25 +32,22 @@ export default withRouter(Write)
 //   };
 
 //   handleOk = e => { 
-//     axios.post(config.serverUrl + "/api/posts", 
-//       {
-  //       paragraph: this.state.paragraph,
-  //       affectivity: this.state.affectivity,
-//      },
-//      {
-      //   headers: {
-      //       'Content-Type': 'application/json',
-  //    })
-//   헤더에 토큰 담기 로직 확인 필요.
-// //     .then((response) => {       
-//       console.log(this.state)  
-//       alert("당신의 소중한 하루가 저장되었습니다.")  
+//     axios.post(config.aiServer + "/summary", {
+//       // paragraph: this.state.paragraph,
+//       // affectivity: this.state.affectivity,
+//       text: [this.state.paragraph],
+//       emotion: Number(this.state.affectivity),
+//     })
+//     .then((response) => {       
+//       console.log(response) 
 //       this.setState({
-//         visible: false,
-//         paragraph: "",
-//         affectivity: "",     
+//         visible: true,
+//         // paragraph: "",
+//         // affectivity: "",
+//         keyword: response.data.keyword,
+//         onesentence: response.data.onesentence,       
 //       })
-//       this.props.history.push('/posts')       
+//       // this.props.history.push('/posts')       
 //     })
 //     .catch((error) => {
 //       alert("에러 발생: " + error.message)
@@ -189,10 +87,11 @@ export default withRouter(Write)
 //           onChange={this.paragraphChanged}  />
 //         <div className="one-post-btn-container flex">
 //           <Emotion clickHandler={this.selectedEmotion}/>
-//           <Button type="primary" onClick={this.showModal} className="btn btn-submit">저장</Button>
+//           <Button type="primary" onClick={this.handleOk} className="btn btn-submit">저장</Button>
 //           <Modal title="글이 완성되었습니다." visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel} >
 // {/*  리뷰 페이지로 이동 */}
-//               저장하시겠습니까?
+//             {"키워드: " + this.state.keyword + "    \n" +
+//              "한줄: " + this.state.onesentence}
 //           </Modal>       
 //         </div>
 //       </div>
@@ -200,3 +99,101 @@ export default withRouter(Write)
 //   }
 // }
 // export default withRouter(Write) 
+
+
+
+// server랑 연결 원본
+import React, { Component } from 'react';
+import { Input, Button, Modal } from 'antd';
+import LiveClock from './LiveClock';
+import Emotion from './Emotion';
+import { withRouter } from 'react-router';
+import axios from 'axios';
+
+const { TextArea } = Input;
+const config = require('../../../config');
+
+class Write extends Component {
+  constructor(props) {
+    super(props) 
+
+    this.state = {
+      visible: false,
+      paragraph: "",
+      affectivity: "",
+    }
+    this.paragraphChanged = this.paragraphChanged.bind(this)
+    this.selectedEmotion = this.selectedEmotion.bind(this)
+  }  
+  
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => { 
+    axios.post(config.serverUrl + "/api/posts", {
+      paragraph: this.state.paragraph,
+      affectivity: this.state.affectivity,
+     })
+  // 헤더에 토큰 담기 로직  필요.
+     .then((response) => {       
+      console.log(this.state)  
+      alert("당신의 소중한 하루가 저장되었습니다.")  
+      this.setState({
+        visible: false,
+        paragraph: "",
+        affectivity: "",     
+      })
+      this.props.history.push('/posts')       
+    })
+    .catch((error) => {
+      alert("에러 발생: " + error.message)
+      console.error(error)
+    })
+  }
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  paragraphChanged(event) {
+    this.setState({
+      paragraph: event.target.value,
+    })
+  }
+
+  selectedEmotion(e) {
+    this.setState({
+      affectivity: e.target.value
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="one-post-write">
+        <div className="one-liveClock-container">
+          <LiveClock />
+        </div> 
+        <TextArea className="one-textarea" 
+          placeholder="...그래서 오늘은 어땠어?"
+          value={this.state.paragraph}
+          onChange={this.paragraphChanged}  />
+        <div className="one-post-btn-container flex">
+          <Emotion clickHandler={this.selectedEmotion}/>
+          <Button type="primary" onClick={this.showModal} className="btn btn-submit">저장</Button>
+          <Modal title="글이 완성되었습니다." visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel} >
+{/*  리뷰 페이지로 이동 */}
+              저장하시겠습니까?
+          </Modal>       
+        </div>
+      </div>
+    )
+  }
+}
+export default withRouter(Write) 
